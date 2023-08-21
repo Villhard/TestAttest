2b78804546a7d11bce39d3162da69a2fb780746b
Victor Ryabov <victorwillhard@gmail.com>
2023-08-21 23:21:25 +0300

    rename function check_user to get_user

--- database/user_connect.py
+++ database/user_connect.py
@@ -8,8 +8,8 @@ from database.database import User, engine
 Session = sessionmaker(engine)
 
 
-def check_user_exists(tg_id: int) -> User | None:
-    """Проверка на существование пользователя в базе данных."""
+def get_user(tg_id: int) -> User | None:
+    """Получение пользователя из базы данных."""
     with Session() as session:
         user = session.query(User).filter(User.tg_id == tg_id).first()
         return user
